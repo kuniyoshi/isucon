d607ef1e86f890e78e1b470ac8b7f3d4c60c0979
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2012-10-13 19:27:13 -0400

    Change to my coding style.

--- webapp/perl/lib/Isucon.pm
+++ webapp/perl/lib/Isucon.pm
@@ -1,20 +1,24 @@
 package Isucon;
-
+use utf8;
 use strict;
 use warnings;
-use utf8;
-use Kossy;
 use DBI;
 use JSON;
+use Kossy;
 
 our $VERSION = 0.01;
 
 sub load_config {
     my $self = shift;
-    return $self->{_config} if $self->{_config};
-    open( my $fh, '<', $self->root_dir . '/../config/hosts.json' ) or die $!;
-    local $/;
-    my $json = <$fh>;
+
+    return $self->{_config}
+        if $self->{_config};
+
+    open my $fh, '<', $self->root_dir . '/../config/hosts.json'
+        or die $!;
+
+    my $json = do { local $/; <$fh> };
+
     $self->{_config} = decode_json( $json );
 }
 
@@ -22,7 +26,8 @@ sub dbh {
     my $self   = shift;
     my $config = $self->load_config;
     my $host   = $config->{servers}->{database}->[0] || '127.0.0.1';
-    DBI->connect_cached(
+
+    return DBI->connect_cached(
         'dbi:mysql:isucon;host=' . $host,
         'isuconapp',
         'isunageruna',
@@ -38,61 +43,56 @@ sub dbh {
 
 filter 'recent_commented_articles' => sub {
     my $app = shift;
-    sub {
-        my ( $self, $c ) = @_;
-        $c->stash->{recent_commented_articles} = $self->dbh->selectall_arrayref(
-'SELECT a.id, a.title FROM comment c INNER JOIN article a ON c.article = a.id 
-            GROUP BY a.id ORDER BY MAX(c.created_at) DESC LIMIT 10',
-            { Slice => {} }
-        );
+    return sub {
+        my( $self, $c ) = @_;
+        $c->stash->{recent_commented_articles}
+            = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} } );
+SELECT a.id, a.title FROM comment c INNER JOIN article a ON c.article = a.id GROUP BY a.id ORDER BY MAX(c.created_at) DESC LIMIT 10
+END_SQL
         $app->( $self, $c );
-      }
+    };
 };
 
-get '/' => [qw/recent_commented_articles/] => sub {
-    my ( $self, $c ) = @_;
-    my $rows = $self->dbh->selectall_arrayref(
-'SELECT id,title,body,created_at FROM article ORDER BY id DESC LIMIT 10',
-        { Slice => {} }
-    );
+get '/' => [ qw/recent_commented_articles/ ] => sub {
+    my( $self, $c ) = @_;
+    my $rows = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} } );
+SELECT id, title, body, created_at FROM article ORDER BY id DESC LIMIT 10
+END_SQL
     $c->render( 'index.tx', { articles => $rows } );
 };
 
-get '/article/:articleid' => [qw/recent_commented_articles/] => sub {
-    my ( $self, $c ) = @_;
-    my $article = $self->dbh->selectrow_hashref(
-        'SELECT id,title,body,created_at FROM article WHERE id=?',
-        {}, $c->args->{articleid} );
-    my $comments = $self->dbh->selectall_arrayref(
-        'SELECT name,body,created_at FROM comment WHERE article=? ORDER BY id',
-        { Slice => {} },
-        $c->args->{articleid}
-    );
+get '/article/:articleid' => [ qw/recent_commented_articles/ ] => sub {
+    my( $self, $c ) = @_;
+    my $article = $self->dbh->selectrow_hashref( <<END_SQL, { }, $c->args->{articleid} );
+SELECT id, title, body, created_at FROM article WHERE id=?
+END_SQL
+    my $comments
+        = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} }, $c->args->{articleid} );
+SELECT name, body, created_at FROM comment WHERE article = ? ORDER BY id
+END_SQL
     $c->render( 'article.tx', { article => $article, comments => $comments } );
 };
 
-get '/post' => [qw/recent_commented_articles/] => sub {
-    my ( $self, $c ) = @_;
+get '/post' => [ qw/recent_commented_articles/ ] => sub {
+    my( $self, $c ) = @_;
     $c->render( 'post.tx' );
 };
 
 post '/post' => sub {
-    my ( $self, $c ) = @_;
-    my $sth =
-      $self->dbh->prepare( 'INSERT INTO article SET title = ?, body = ?' );
+    my( $self, $c ) = @_;
+    my $sth = $self->dbh->prepare( 'INSERT INTO article SET title = ?, body = ?' );
     $sth->execute( $c->req->param( 'title' ), $c->req->param( 'body' ) );
     $c->redirect( $c->req->uri_for( '/' ) );
 };
 
 post '/comment/:articleid' => sub {
-    my ( $self, $c ) = @_;
+    my( $self, $c ) = @_;
 
-    my $sth = $self->dbh->prepare(
-        'INSERT INTO comment SET article = ?, name =?, body = ?' );
+    my $sth = $self->dbh->prepare( 'INSERT INTO comment SET article = ?, name =?, body = ?' );
     $sth->execute(
         $c->args->{articleid},
         $c->req->param( 'name' ),
-        $c->req->param( 'body' )
+        $c->req->param( 'body' ),
     );
     $c->redirect( $c->req->uri_for( '/article/' . $c->args->{articleid} ) );
 };
