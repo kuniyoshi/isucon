6ab83a508a0794cb6ad5feac87954faa86b389e6
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2012-11-28 10:54:06 -0500

    tidy up

--- webapp/perl/lib/Isucon.pm
+++ webapp/perl/lib/Isucon.pm
@@ -2,6 +2,7 @@ package Isucon;
 use utf8;
 use strict;
 use warnings;
+use autodie qw( open close );
 use DBI;
 use JSON;
 use Kossy;
@@ -11,29 +12,26 @@ my $MEMD = Cache::Memcached->new(
     servers => [ "192.168.1.122:11211" ],
 );
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
-sub load_config {
+sub config {
     my $self = shift;
 
-    return $self->{_config}
-        if $self->{_config};
-
-    open my $fh, '<', $self->root_dir . '/../config/hosts.json'
-        or die $!;
-
-    my $json = do { local $/; <$fh> };
-
-    $self->{_config} = decode_json( $json );
+    return $self->{_config} ||= do {
+        open my $fh, '<', $self->root_dir . '/../config/hosts.json';
+        my $json = do { local $/; <$fh> };
+        close $fh;
+        decode_json( $json );
+    };
 }
 
 sub dbh {
     my $self   = shift;
-    my $config = $self->load_config;
-    my $host   = $config->{servers}->{database}->[0] || '127.0.0.1';
+    my $config = $self->config;
+    my $host   = $config->{servers}{database}[0] || '127.0.0.1';
 
     return DBI->connect_cached(
-        'dbi:mysql:isucon;host=' . $host,
+        "dbi:mysql:isucon;host=$host",
         'isuconapp',
         'isunageruna',
         {
@@ -41,8 +39,8 @@ sub dbh {
             PrintError          => 0,
             ShowErrorStatement  => 1,
             AutoInactiveDestroy => 1,
-            mysql_enable_utf8   => 1
-        }
+            mysql_enable_utf8   => 1,
+        },
     );
 }
 
@@ -63,17 +61,15 @@ END_SQL
     };
 };
 
-#get '/' => [ qw/recent_commented_articles/ ] => sub {
 get '/' => sub {
     my( $self, $c ) = @_;
     my $rows = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} } );
 SELECT id, title, body, created_at FROM article ORDER BY id DESC LIMIT 10
 END_SQL
-    $c->render( 'index.tx', { articles => $rows } );
+    return $c->render( 'index.tx', { articles => $rows } );
 };
 
 get '/article/:articleid' => sub {
-#get '/article/:articleid' => [ qw/recent_commented_articles/ ] => sub {
     my( $self, $c ) = @_;
     my $article = $self->dbh->selectrow_hashref( <<END_SQL, { }, $c->args->{articleid} );
 SELECT id, title, body, created_at FROM article WHERE id=?
@@ -82,20 +78,19 @@ END_SQL
         = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} }, $c->args->{articleid} );
 SELECT name, body, created_at FROM comment WHERE article = ? ORDER BY id
 END_SQL
-    $c->render( 'article.tx', { article => $article, comments => $comments } );
+    return $c->render( 'article.tx', { article => $article, comments => $comments } );
 };
 
 get '/post' => sub {
-#get '/post' => [ qw/recent_commented_articles/ ] => sub {
     my( $self, $c ) = @_;
-    $c->render( 'post.tx' );
+    return $c->render( 'post.tx' );
 };
 
 post '/post' => sub {
     my( $self, $c ) = @_;
     my $sth = $self->dbh->prepare( 'INSERT INTO article SET title = ?, body = ?' );
     $sth->execute( $c->req->param( 'title' ), $c->req->param( 'body' ) );
-    $c->redirect( $c->req->uri_for( '/' ) );
+    return $c->redirect( $c->req->uri_for( '/' ) );
 };
 
 post '/comment/:articleid' => sub {
@@ -132,26 +127,12 @@ my $res = $ua->request(
 );
 warn $res->code, " - PURGE $url";
 
-    $c->redirect( $c->req->uri_for( '/article/' . $c->args->{articleid} ) );
+    return $c->redirect( $c->req->uri_for( '/article/' . $c->args->{articleid} ) );
 };
 
 get '/side' => [ qw/recent_commented_articles/ ] => sub {
     my( $self, $c ) = @_;
-    $c->render( 'side.tx' );
+    return $c->render( 'side.tx' );
 };
 
-get '/probe' => sub { "ok" };
-
-### filter 'recent_commented_articles' => sub {
-###     my $app = shift;
-###     return sub {
-###         my( $self, $c ) = @_;
-###         $c->stash->{recent_commented_articles}
-###             = $self->dbh->selectall_arrayref( <<END_SQL, { Slice => {} } );
-### SELECT a.id, a.title FROM comment c INNER JOIN article a ON c.article = a.id GROUP BY a.id ORDER BY MAX(c.created_at) DESC LIMIT 10
-### END_SQL
-###         $app->( $self, $c );
-###     };
-### };
 1;
-
